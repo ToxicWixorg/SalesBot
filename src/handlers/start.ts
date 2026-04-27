05b962c55773d5abef8a55ebfe341f19b33d80ee
ToxicWixorg <m.salehifar980@gmail.com>
2026-04-28 01:19:19 +0330

    update

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -5,6 +5,6 @@ import { text } from "drizzle-orm/sqlite-core/columns/text";
 export const startComposer = new Composer()
   .extend(composer)
   .command("start", (context) => {
-    const text = `Welcome to the bot! Use /help to see available commands.\nid: ${context.from?.id}\nusername: @${context.from?.username}`;
+    const text = `Welcome to the bot! Use /help to see available commands.\nid: ${context.from?.id}\nusername: @${context.from?.username}\nchatId: ${context.chat.id}`;
     context.send(text);
   });
