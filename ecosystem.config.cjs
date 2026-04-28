66f0fe9b8492b308200dfc6a4d93c3d0c5936022
ToxicWixorg <m.salehifar980@gmail.com>
2026-04-28 16:10:35 +0330

    update

---
+++ ecosystem.config.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [
+    {
+      name: "sales-bot",
+      script: "bun",
+      args: "run dev",
+      cwd: "/root/bot",
+      autorestart: true,
+      watch: false,
+      max_memory_restart: "1G",
+      error_file: "logs/error.log",
+      out_file: "logs/out.log",
+      time: true,
+    },
+  ],
+};
