f381ed6d570d858a1347e2db67e02a9a614e54db
ToxicWixorg <m.salehifar980@gmail.com>
2026-04-28 15:13:31 +0330

    update

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,17 @@
 import env from "env-var";
 
 export const config = {
-	NODE_ENV: env
-		.get("NODE_ENV")
-		.default("development")
-		.asEnum(["production", "test", "development"]),
-	BOT_TOKEN: env.get("BOT_TOKEN").required().asString(),
+  NODE_ENV: env
+    .get("NODE_ENV")
+    .default("development")
+    .asEnum(["production", "test", "development"]),
+  BOT_TOKEN: env.get("BOT_TOKEN").required().asString(),
 
-	DATABASE_URL: env.get("DATABASE_URL").required().asString(),
-	REDIS_HOST: env.get("REDIS_HOST").default("localhost").asString(),
-	LOCK_STORE: env
-		.get("LOCK_STORE")
-		.default("memory")
-		.asEnum(["memory", "redis"]),
+  DATABASE_URL: env.get("DATABASE_URL").required().asString(),
+  REDIS_HOST: env.get("REDIS_HOST").default("localhost").asString(),
+  REDIS_PORT: env.get("REDIS_PORT").default("6379").asPortNumber(),
+  LOCK_STORE: env
+    .get("LOCK_STORE")
+    .default("memory")
+    .asEnum(["memory", "redis"]),
 };
--- src/services/redis.ts
+++ src/services/redis.ts
@@ -2,7 +2,8 @@ import { Redis } from "ioredis";
 import { config } from "../config.ts";
 
 export const redis = new Redis({
-	host: config.REDIS_HOST,
-	// for bullmq
-	maxRetriesPerRequest: null,
+  host: config.REDIS_HOST,
+  port: config.REDIS_PORT,
+  // for bullmq
+  maxRetriesPerRequest: null,
 });
