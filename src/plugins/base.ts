5799fef831085a87eaf0714a30882fe980c794fa
ToxicWixorg <m.salehifar980@gmail.com>
2026-04-28 18:45:54 +0330

    update

--- src/plugins/base.ts
+++ src/plugins/base.ts
@@ -1,11 +1,24 @@
 import { autoAnswerCallbackQuery } from "@gramio/auto-answer-callback-query";
 import { autoRetry } from "@gramio/auto-retry";
-import { redisStorage } from "@gramio/storage-redis";
 import { Composer } from "gramio";
 import { redis } from "../services/redis.ts";
 import { i18n } from "../shared/locales/index.ts";
 
-export const storage = redisStorage(redis);
+export const storage = {
+  async get(key: string) {
+    const data = await redis.get(key);
+    return data ? JSON.parse(data) : undefined;
+  },
+  async has(key: string) {
+    return (await redis.exists(key)) === 1;
+  },
+  async set(key: string, value: unknown) {
+    await redis.set(key, JSON.stringify(value));
+  },
+  async delete(key: string) {
+    return (await redis.del(key)) === 1;
+  },
+};
 
 export const baseComposer = new Composer({ name: "base" })
   .extend(autoAnswerCallbackQuery())
