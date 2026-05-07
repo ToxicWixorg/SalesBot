c378c243dce5c2f05c6374183dd2d9ba83572b8b
ToxicWixorg <m.salehifar980@gmail.com>
2026-05-07 14:19:27 +0330

    update

--- src/handlers/products/callbacks/ProductsCallback.ts
+++ src/handlers/products/callbacks/ProductsCallback.ts
@@ -2,4 +2,5 @@ import { CategoryRepository } from "../../../repositories/ProductRepository.ts";
 
 export async function ProductsCallback(context: any) {
   const categories = await CategoryRepository.findAll();
+  console.log("[CATEGORIES] : ", categories);
 }
--- src/handlers/products/products.ts
+++ src/handlers/products/products.ts
@@ -1,14 +1,11 @@
 import { Composer } from "gramio";
 import { composer } from "../../plugins/index.ts";
+import { ProductsCallback } from "./callbacks/ProductsCallback.ts";
 
 export const inviteComposer = new Composer()
   .extend(composer)
   .callbackQuery("products", async (context) => {
     return await ProductsCallback(context);
   })
-  .callbackQuery(/copy_invite_(.+)/, async (context) => {
-    return await CopyInviteCallback(context);
-  })
-  .callbackQuery("view_referrals", async (context) => {
-    return await ViewReferralsCallBack(context);
+
   });
