12eb2fc63e228f1c160267756c16cc2e9ddc7d69
ToxicWixorg <m.salehifar980@gmail.com>
2026-05-08 12:59:24 +0330

    update

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,13 +1,11 @@
 import { scenes, scenesDerives } from "@gramio/scenes";
 import { Composer } from "gramio";
-import { greetingScene } from "../scenes/greeting.ts";
 import { languageSelectionScene } from "../scenes/language-selection.ts";
 import { enterDiscountCodeScene } from "../scenes/enter-discount-code.ts";
 import { enterDiscountCodeOrderScene } from "../scenes/enter-discount-code-order.ts";
 import { baseComposer, storage } from "./base.ts";
 
 const scenesList = [
-  greetingScene,
   languageSelectionScene,
   enterDiscountCodeScene,
   enterDiscountCodeOrderScene,
