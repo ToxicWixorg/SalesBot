92c4dcb98b0e5797cf3d6d69371cd54f9a4305d7
ToxicWixorg <m.salehifar980@gmail.com>
2026-05-12 20:25:37 +0330

    update

--- src/handlers/products/callbacks/ProductsCallback.ts
+++ src/handlers/products/callbacks/ProductsCallback.ts
@@ -3,7 +3,10 @@ import { UserRepository } from "../../../repositories/UserRepository.ts";
 import { i18n } from "../../../shared/locales/index.ts";
 import { getBotSettings } from "../../../plugins/base.ts";
 import { CategoryRepository } from "../../../repositories/ProductRepository.ts";
-import { categoriesKeyboard } from "../../../shared/keyboards/index.ts";
+import {
+  backToMainKeyboard,
+  categoriesKeyboard,
+} from "../../../shared/keyboards/index.ts";
 
 export async function ProductsCallback(context: Context) {
   if (!context.from) return;
@@ -16,12 +19,17 @@ export async function ProductsCallback(context: Context) {
   const settings = await getBotSettings();
   if (!settings.shopEnabled) {
     await context.send(t("shopDisabled"), { parse_mode: "HTML" });
+    await context.message?.delete();
     return;
   }
 
   const categories = await CategoryRepository.findAll();
   if (categories.length === 0) {
-    await context.send(t("noProducts"), { parse_mode: "HTML" });
+    await context.send(t("noProducts"), {
+      reply_markup: backToMainKeyboard(t),
+      parse_mode: "HTML",
+    });
+    await context.message?.delete();
     return;
   }
 
@@ -29,4 +37,5 @@ export async function ProductsCallback(context: Context) {
     parse_mode: "HTML",
     reply_markup: categoriesKeyboard(t, categories),
   });
+  await context.message?.delete();
 }
