00f88ccd9689678ea4197edd8c4cc60c56b008df
ToxicWixorg <m.salehifar980@gmail.com>
2026-06-30 20:11:21 +0330

    update

--- src/repositories/ProductRepository.ts
+++ src/repositories/ProductRepository.ts
@@ -6,7 +6,7 @@ import {
   productPlansTable,
   ProductPlan,
   InsertProductPlan,
-  premiumCategoriesTable as categoriesTable,ثس
+  premiumCategoriesTable as categoriesTable,
   Category,
   InsertCategory,
   productConfigsTable,
